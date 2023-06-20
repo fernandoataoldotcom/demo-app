7dc73760bd158e31ce8dc8d76a1afe91a235c7ca
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-06-20 20:31:26 +0000

    refactor: rename

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("fernandoataoldotcom is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("fernandoataoldotcom is decent!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
